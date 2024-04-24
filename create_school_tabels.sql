011410cc9dcf4c9f9caf03bb351fb3fdfd71fc19
Felix Berger <felix.enok.berger@gmail.com>
2024-04-24 10:29:53 +0200

    Added fag column to Underviser table

--- create_school_tabels.sql
+++ create_school_tabels.sql
@@ -1,41 +1,42 @@
 -- Create table Elev
 CREATE TABLE Elev (
-  elevid INT PRIMARY KEY,
-  fornavn NVARCHAR(255) NOT NULL,
-  efternavn NVARCHAR(255) NOT NULL,
-  adresse NVARCHAR(255) NOT NULL,
-  postnr INT,
-  klasseid INT,
-  FOREIGN KEY (postnr) REFERENCES PostNrBy(postnr),
-  FOREIGN KEY (klasseid) REFERENCES Klasse(klasseid)
+    elevid INT PRIMARY KEY,
+    fornavn NVARCHAR(255) NOT NULL,
+    efternavn NVARCHAR(255) NOT NULL,
+    adresse NVARCHAR(255) NOT NULL,
+    postnr INT,
+    klasseid INT,
+    FOREIGN KEY (postnr) REFERENCES PostNrBy(postnr),
+    FOREIGN KEY (klasseid) REFERENCES Klasse(klasseid)
 );
 
 -- Create table Laerer
 CREATE TABLE Laerer (
-  laererid INT PRIMARY KEY,
-  fornavn NVARCHAR(255) NOT NULL,
-  efternavn NVARCHAR(255) NOT NULL,
-  adresse NVARCHAR(255) NOT NULL,
-  postnr INT,
-  FOREIGN KEY (postnr) REFERENCES PostNrBy(postnr),
+    laererid INT PRIMARY KEY,
+    fornavn NVARCHAR(255) NOT NULL,
+    efternavn NVARCHAR(255) NOT NULL,
+    adresse NVARCHAR(255) NOT NULL,
+    postnr INT,
+    FOREIGN KEY (postnr) REFERENCES PostNrBy(postnr),
 );
 
 -- Create table Klasse
 CREATE TABLE Klasse (
-  klasseid INT PRIMARY KEY,
-  klassenavn NVARCHAR(255) NOT NULL
+    klasseid INT PRIMARY KEY,
+    klassenavn NVARCHAR(255) NOT NULL
 );
 
 -- Create table PostNrBy
 CREATE TABLE PostNrBy (
-  postnr INT PRIMARY KEY,
-  bynavn NVARCHAR(255) NOT NULL
+    postnr INT PRIMARY KEY,
+    bynavn NVARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Underviser (
-  laererid INT,
-  klasseid INT,
-  FOREIGN KEY (laererid) REFERENCES Laerer(laererid),
-  FOREIGN KEY (klasseid) REFERENCES Klasse(klasseid),
-  PRIMARY KEY (laererid, klasseid)
+    fag NVARCHAR(255) NOT NULL,
+    laererid INT,
+    klasseid INT,
+    FOREIGN KEY (laererid) REFERENCES Laerer(laererid),
+    FOREIGN KEY (klasseid) REFERENCES Klasse(klasseid),
+    PRIMARY KEY (laererid, klasseid, fag)
 );
